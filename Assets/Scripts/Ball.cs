1c3e568974590d342cf03a742ac805f1b1a54c99
Tarık Köprülü <tarikk3545@gmail.com>
2018-11-27 22:21:56 +0300

    Added walls and small fixes

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    [SerializeField] Paddle paddle;
+    [SerializeField] float bouncePowerY = 12f;
+    [SerializeField] float bouncePowerX = 0f;
+
+    Vector2 paddleDistance;
+
+    bool isStarted = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        paddleDistance = transform.position - paddle.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(!isStarted)
+        {           
+            LockBallToPaddle();
+            LaunchOnClick();
+        }
+        
+    }
+
+    private void LaunchOnClick()
+    {
+        // button0 = Left Mouse
+        if(Input.GetMouseButtonDown(0))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(bouncePowerX, bouncePowerY);
+            isStarted = true;
+        }
+    }
+
+    private void LockBallToPaddle()
+    {
+        Vector2 paddlePos = new Vector2(paddle.transform.position.x, paddle.transform.position.y);
+        transform.position = paddlePos + paddleDistance;
+    }
+}
---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour {
+
+    [SerializeField] float screenWidthUnits;
+    [SerializeField] float minimumX = 1.75f;
+    [SerializeField] float maximumX = 14.25f;
+	// Update is called once per frame
+	void Update () {
+        float mousePos = (Input.mousePosition.x / Screen.width) * screenWidthUnits;
+        Vector2 vectorPos = new Vector2(transform.position.x, transform.position.y);
+        vectorPos.x = Mathf.Clamp(mousePos,minimumX,maximumX); 
+        transform.position = vectorPos;
+	}
+}
